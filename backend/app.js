01d3b89d97f5a5c1caba6c5676fe50541641bef2
s777iax <soniaykm@gmail.com>
2026-01-01 18:38:06 +0800

    Add CORS middleware for API access

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
+import express from 'express';
+import cors from 'cors';
 import incidentRouter from './routes/incident.route.js';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import express from 'express';
+
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRouter);
