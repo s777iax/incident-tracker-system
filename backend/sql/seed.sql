e2b8ce736c1f663a70b2955167ea11946eaebc76
s777iax <soniaykm@gmail.com>
2026-01-02 17:46:26 +0800

    Update test users

--- backend/sql/seed.sql
+++ backend/sql/seed.sql
@@ -1,9 +1,14 @@
 BEGIN;
 
+-- =========================
+-- Test Users (For Demo Only)
+-- Password for all accounts: myPassword123
+-- =========================
+
 INSERT INTO public.users (email, "role", password_hash) VALUES
-('sally@email.com', 'admin', '$2b$10$GYujkoFILKQPc4tJMoLo0Og37kso13DovmRFfb8p0kemepSudR6Ra'),
-('john@email.com', 'user', '$2b$10$02BMt3KZfITEFMX54TD7IOxIRHOfEv/QcRrmIUPDggRDqsX2TDnei'),
-('jane@email.com', 'user', '$2b$10$/AqCkj.78O/FgKskZwbM8.X1Opz8qJMqcJC3Qi/TD2ck.M93Cbgui');
+('sally@email.com', 'admin', '$2b$10$.5bcCndJ9d2U9tkOq1cYhOirkObBV.0Aay.YJSH0h3EkiL2dsMQO.'),
+('john@email.com', 'user', '$2b$10$sua7q.457.6d9oT1NC0MRu38wAWvwaFGWGpLtO1Zqj4NABu5Nt3h2'),
+('jane@email.com', 'user', '$2b$10$wdDisDAMEIbK/sNlgdobreeJHhrzopxBMhsr8BwzzQRCDxEC8H8pa');
 
 INSERT INTO public.incidents (title, description, status, created_by, ai_summary) VALUES
 ('Bus crash', 'Just now there was this super loud crash sound and I didn''t even know what it was at first. Then I looked over at Jurong West Avenue 1 and realised bus 98 and bus 99 somehow crashed into each other. It all happened quite fast so I was lowkey confused what I was seeing. The glass was all shattered everywhere which made it look quite bad, and a lot of people inside seemed injured or at least very shocked. Everyone was kind of standing around not really knowing what to do, like the whole scene just felt chaotic and blur for a while.', 'OPEN', 2, NULL),
