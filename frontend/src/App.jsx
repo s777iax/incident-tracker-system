9d70effede49d2497280ee8604a4c41e18512f2d
s777iax <soniaykm@gmail.com>
2026-01-03 20:48:05 +0800

    Updated routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,54 +4,41 @@ import Register from "./pages/Register";
 import Incidents from "./pages/Incidents";
 import IncidentDetails from "./pages/IncidentDetails";
 import IncidentForm from "./pages/IncidentForm";
-import Dashboard from "./pages/Dashboard.jsx";
-import Navbar from './components/Navbar.jsx';
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const admin = JSON.parse(localStorage.getItem("user"))?.role === "admin";
   const token = localStorage.getItem("token");
 
   return (
     <>
       {token && <Navbar />}
+
       <Routes>
-        <Route
-          path="/login"
-          element={
-            token ? (
-              <Navigate
-                to={admin ? "/dashboard" : "/incidents"}
-                replace
-              />
-            ) : (
-              <Login />
-            )
-          }
-        />
-        <Route
-          path="/register"
-          element={token ? <Navigate to="/incidents" /> : <Register />}
-        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
         <Route
           path="/incidents"
           element={token ? <Incidents /> : <Navigate to="/login" />}
         />
+
         <Route
           path="/incidents/:id"
           element={token ? <IncidentDetails /> : <Navigate to="/login" />}
         />
+
         <Route
           path="/incidents/new"
           element={token ? <IncidentForm /> : <Navigate to="/login" />}
         />
+
         <Route
           path="/dashboard"
           element={token ? <Dashboard /> : <Navigate to="/login" />}
         />
-        <Route
-          path="*"
-          element={<Navigate to={token ? "/incidents" : "/login"} />}
-        />
+
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </>
   );
